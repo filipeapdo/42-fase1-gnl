41ab0c80b0cdd6f56f9a9e71351207f27e10a4e2
fiaparec <fiaparec@student.42sp.org.br>
2022-02-20 12:33:17 -0300

    gnl, ready to delivery: try 1

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fiaparec <fiaparec@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 10:06:18 by fiaparec          #+#    #+#             */
-/*   Updated: 2022/02/16 08:08:26 by fiaparec         ###   ########.fr       */
+/*   Updated: 2022/02/20 12:28:28 by fiaparec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,16 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+// get_next_line
 char	*get_next_line(int fd);
+char	*flp_read_chunck(int fd, char *chunck);
+char	*flp_get_line(char *chunck);
+char	*flp_new_chunck(char *chunck);
+int		flp_check_nl(char *chunck);
+
+// get_next_line_utils
 char	*flp_gnljoin(char *s1, const char *s2);
-char	*ft_strdup(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,39 +6,12 @@
 /*   By: fiaparec <fiaparec@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 10:59:18 by fiaparec          #+#    #+#             */
-/*   Updated: 2022/02/18 17:52:02 by fiaparec         ###   ########.fr       */
+/*   Updated: 2022/02/20 12:28:30 by fiaparec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-//	BUFFER_SIZE = 1
-// 		real    0m9.957s
-// 		user    0m9.886s
-// 		sys     0m0.070s
-// char	*flp_gnljoin(char *s1, char const *s2)
-// {
-// 	char	*s_join;
-
-// 	if (!s1 && !s2)
-// 		return (NULL);
-// 	if (!s1)
-// 		return (ft_strdup(s2));
-// 	if (!s2)
-// 		return (ft_strdup(s1));
-// 	s_join = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-// 	if (!s_join)
-// 		return (NULL);
-// 	ft_strlcpy(s_join, s1, ft_strlen(s1) + 1);
-// 	ft_strlcat(s_join, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
-// 	free(s1);
-// 	return (s_join);
-// }
-
-//	BUFFER_SIZE = 1
-// 		real    0m5.491s
-// 		user    0m5.441s
-// 		sys     0m0.051s
 char	*flp_gnljoin(char *s1, const char *s2)
 {
 	size_t	i;
@@ -67,19 +40,6 @@ char	*flp_gnljoin(char *s1, const char *s2)
 	return (s_join);
 }
 
-char	*ft_strdup(const char *s)
-{
-	char	*s_dup;
-
-	if (!s)
-		return (NULL);
-	s_dup = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
-	if (!s_dup)
-		return (NULL);
-	ft_strlcpy(s_dup, s, ft_strlen(s) + 1);
-	return (s_dup);
-}
-
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	int	i;
@@ -95,31 +55,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 		*(dst + i) = '\0';
 	}
 	return (ft_strlen(src));
-}
-
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-	int		i;
-	size_t	len_src;
-	size_t	len_dst;
-
-	i = 0;
-	len_src = ft_strlen(src);
-	len_dst = ft_strlen(dst);
-	if (dstsize == 0)
-		return (len_src);
-	else if (len_dst >= dstsize)
-		return (len_src + dstsize);
-	else
-	{
-		while (*(src + i) && ((dstsize--) - len_dst) > 1)
-		{
-			*(dst + len_dst + i) = *(src + i);
-			i++;
-		}
-		*(dst + len_dst + i) = '\0';
-	}
-	return (len_src + len_dst);
 }
 
 size_t	ft_strlen(const char *s)
